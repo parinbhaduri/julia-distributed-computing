e0abea409f658164b1543487e2a5261caf737f27
parinbhaduri <87945724+parinbhaduri@users.noreply.github.com>
2023-10-15 17:09:39 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=parallel_test_julia
-#SBATCH --nodes 1
-#SBATCH --n-tasks-per-node 40
+#SBATCH --nodes=1
+#SBATCH --n-tasks-per-node=40
 #SBATCH --exclusive
 
 # Print properties of job as submitted
