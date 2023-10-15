79a18cd6b5b8bfc17726fe210093be6ccdd2f278
parinbhaduri <87945724+parinbhaduri@users.noreply.github.com>
2023-10-15 16:53:46 -0400

    Update main.sh
    Added property views based on SLURM commands

--- main.sh
+++ main.sh
@@ -4,6 +4,19 @@
 #SBATCH --n-tasks-per-node 40
 #SBATCH --exclusive
 
+# Print properties of job as submitted
+echo "SLURM_JOB_ID = $SLURM_JOB_ID"
+echo "SLURM_NTASKS = $SLURM_NTASKS"
+echo "SLURM_NTASKS_PER_NODE = $SLURM_NTASKS_PER_NODE"
+echo "SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK"
+echo "SLURM_JOB_NUM_NODES = $SLURM_JOB_NUM_NODES"
+
+# Print properties of job as scheduled by Slurm
+echo "SLURM_JOB_NODELIST = $SLURM_JOB_NODELIST"
+echo "SLURM_TASKS_PER_NODE = $SLURM_TASKS_PER_NODE"
+echo "SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE"
+echo "SLURM_CPUS_ON_NODE = $SLURM_CPUS_ON_NODE"
+
 # Load the Julia module
 module load julia/1.6.2
 
