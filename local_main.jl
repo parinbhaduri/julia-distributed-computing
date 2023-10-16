8a52e700b68347afc4d946d442bce9b7392e0819
parinbhaduri <pbb62@cornell.edu>
2023-10-16 15:21:32 -0400

    added file for running on local machine

---
+++ local_main.jl
@@ -0,0 +1,57 @@
+#For use on local machine
+using Distributed
+
+num_cores = parse(Int,"4")
+addprocs(num_cores)#, exeflags="--project=$(Base.active_project())")
+
+# instantiate and precompile environment
+@everywhere begin
+  using Pkg; Pkg.activate(@__DIR__); 
+  Pkg.instantiate(); Pkg.precompile()
+end
+
+# load dependencies and helper functions
+@everywhere begin
+  using ProgressMeter
+  using CSV
+
+  function process(infile, outfile)
+    # read file from disk
+    csv = CSV.File(infile)
+
+    # perform calculations
+    sleep(60)
+
+    # save new file to disk
+    CSV.write(outfile, csv)
+  end
+end
+
+# MAIN SCRIPT
+# -----------
+
+# relevant directories
+indir  = joinpath(@__DIR__,"data")
+outdir = joinpath(@__DIR__,"results")
+
+# files to process
+infiles  = readdir(indir, join=true)
+outfiles = joinpath.(outdir, basename.(infiles))
+nfiles   = length(infiles)
+
+# process files in parallel
+status = @showprogress pmap(1:nfiles) do i
+  try
+    process(infiles[i], outfiles[i])
+    true # success
+  catch e
+    false # failure
+  end
+end
+
+# report files that failed
+failed = infiles[.!status]
+if !isempty(failed)
+  println("List of files that failed:")
+  foreach(println, failed)
+end
