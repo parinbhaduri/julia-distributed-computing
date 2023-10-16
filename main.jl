1062b6d73de94c20b82275ce883e06af21f3eeb4
parinbhaduri <87945724+parinbhaduri@users.noreply.github.com>
2023-10-16 14:56:25 -0400

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,11 +1,12 @@
 using Distributed
 
 num_cores = parse(Int,ENV["SLURM_TASKS_PER_NODE"])
-addprocs(num_cores, exeflags="--project=$(Base.active_project())")
+addprocs(num_cores)#, exeflags="--project=$(Base.active_project())")
 
 # instantiate and precompile environment
 @everywhere begin
-  using Pkg; Pkg.instantiate(); Pkg.precompile()
+  using Pkg; Pkg.activate(@__DIR__); 
+  Pkg.instantiate(); Pkg.precompile()
 end
 
 # load dependencies and helper functions
